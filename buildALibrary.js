c5f10709d8f545da438f28c3affb6ee9b91398dd
Tim Weigel <timweigel@gmail.com>
2021-02-27 21:28:53 -0600

    Updating project file and work notes
    Adding Book, Movie, and CD subclasses. Updating work notes.

--- buildALibrary.js
+++ buildALibrary.js
@@ -41,3 +41,46 @@ class Media {
 		this._ratings.push(newRating);
 	}
 }
+
+class Book extends Media {
+	constructor(author, title, pages) {
+		super(title);
+		this._author = author;
+		this._pages = pages;
+	}
+
+	get author() {
+		return this._author;
+	}
+
+	get pages() {
+		return this._pages;
+	}
+}
+
+class Movie extends Media {
+	constructor(director, title, runTime) {
+		super(title);
+		this._director = director;
+		this._runTime = runTime;
+	}
+
+	get director() {
+		return this._director;
+	}
+
+	get runTime() {
+		return this._runTime;
+	}
+}
+
+class CD extends Media {
+	constructor (artist, title) {
+		super(title);
+		this._artist = artist;
+	}
+
+	get artist() {
+		return this._artist;
+	}
+}
