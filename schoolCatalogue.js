41bbc7d95ca1ad65ad06f4586dcbbc6a9a51102a
Tim Weigel <timweigel@gmail.com>
2021-03-06 21:53:51 -0600

    Update working notes and solution
    Update working notes with challenges and tests.
    
    Update solution with progress.

--- schoolCatalogue.js
+++ schoolCatalogue.js
@@ -1,5 +1,70 @@
 class School {
-	constructor(name) {
+	constructor(name, level, numberOfStudents) {
 		this._name = name;
+		this._level= level;
+		this._numberOfStudents = numberOfStudents;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	get level() {
+		return this._level;
+	}
+
+	get numberOfStudents() {
+		return this._numberOfStudents;
+	}
+
+	setNumberOfStudents(newNumberOfStudents) {
+		if (typeof newNumberOfStudents === 'number') {
+			this._numberOfStudents = newNumberOfStudents;
+		} else {
+			console.log('Invalid input: numberOfStudents must be set to a Number.');
+		}
+	}
+
+	quickFacts() {
+		console.log(`${this._name} educates ${this._numberOfStudents} at the ${this._level} school level.`);
+	}
+
+	static pickSubstituteTeacher(substituteTeachers) {
+		let numSubs = substituteTeachers.length;
+		let randIndex;
+		let randomSub;
+		
+		randIndex = Math.floor(Math.random() * (numSubs));
+		randomSub = substituteTeachers[randIndex];
+
+		return randomSub;
+	}
+}
+
+class PrimarySchool extends School {
+	constructor(name, numberOfStudents, pickupPolicy) {
+		super(name, 'primary', numberOfStudents);
+		this._pickupPolicy = pickupPolicy;
+	}
+
+	get pickupPolicy() {
+		returns 
+	}
+}
+
+class MiddleSchool extends School {
+	constructor (name, numberOfStudents){
+		super(name, 'middle', numberOfStudents);
+	}
+}
+
+class HighSchool extends School {
+	constructor(name, numberOfStudents, sportsTeams) {
+		super(name, 'high', numberOfStudents);
+		this._sportsTeams = sportsTeams;
+	}
+
+	get sportsTeams() {
+		console.log(this._sportsTeams);
 	}
 }
